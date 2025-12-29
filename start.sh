8644d915a5e3baa72b95a7e740d5872ed4e7c915
itzdinnertime <chenjason214@gmail.com>
2025-12-29 01:44:25 -0500

    Added startup script

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Start frontend
+cd frontend/
+npm start &
+cd ../
+
+# Detect OS and activate virtual environment accordingly
+if [[ -d "venv/Scripts" ]]; then
+	# Windows (Git Bash, MSYS, Cygwin)
+	source venv/Scripts/activate
+elif [[ -d "venv/bin" ]]; then
+	# Linux/macOS
+	source venv/bin/activate
+else
+	echo "No valid virtual environment found."
+	exit 1
+fi
+
+# Start backend
+uvicorn app.main:app --reload &
